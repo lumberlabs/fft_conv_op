b92d4be1518e4e3408200e1c2a7b8de945a536d2
Joshua Bleecher Snyder <josh@treelinelabs.com>
2010-12-07 18:04:25 -0500

    some hideous timing code

--- cufft_sample.cu
+++ cufft_sample.cu
@@ -4,8 +4,11 @@
 #include <cuda_runtime.h>
 #include <cufft.h>
 #include <cuComplex.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <time.h>
+
 
 // TODO: Why are these necessary? What's wrong with stdint.h??
 typedef unsigned int uint32;
@@ -30,7 +33,7 @@ __global__ void elementwise_vector_scalar_multiply_inplace(cufftReal *vector, fl
 }
 
 uint32 next_power_of_two(uint32 i) {
-    // from Sean Anderson's bit twiddling hacks (TODO: insert link)
+    // from Sean Anderson's bit twiddling hacks at http://www-graphics.stanford.edu/~seander/bithacks.html
     i--;
     i |= i >> 1;
     i |= i >> 2;
@@ -41,16 +44,27 @@ uint32 next_power_of_two(uint32 i) {
     return i;
 }
 
+uint32 max_threads_per_block() {
+    uint32 device = 0;
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, device);
+    int max_threads_per_block = (deviceProp.major >= 2 ? 512 : 256);
+    return max_threads_per_block;
+}
+
 // NB: This will die at runtime sometimes in cuda 3.1 due to
 // data alignment issues -- see http://forums.nvidia.com/index.php?showtopic=176207&mode=linearplus
-// TODO: Don't use it w/ 3.1.
+// TODO: Don't use it w/ 3.1. (How to detect?)
 int main(int argc, char *argv[])
 {
-    uint32 num_images = 2;
+    // general cuda setup and prep
+    int threads_per_block = max_threads_per_block();
+    
+    uint32 num_images = 1;
     uint32 image_rows = 28;
     uint32 image_cols = 28;
 
-    uint32 num_kernels = 3;
+    uint32 num_kernels = 50;
     uint32 kernel_rows = 5;
     uint32 kernel_cols = 5;
 
@@ -77,6 +91,7 @@ int main(int argc, char *argv[])
         }
     }
 
+/*
     fprintf(stderr, "INBOUND IMAGES\n");
     for(uint32 image_index = 0; image_index < num_images; image_index++) {
         for(uint32 r = 0; r < image_rows; r++) {
@@ -99,6 +114,7 @@ int main(int argc, char *argv[])
         }
         fprintf(stderr, "\n");
     }
+*/
 
     // copy images and kernels to device
     // do this first, and simply (before padding etc., to mimic what theano
@@ -110,6 +126,39 @@ int main(int argc, char *argv[])
     cufftReal *inbound_kernels;
     cudaMalloc((void**)&inbound_kernels, sizeof(cufftReal) * num_kernels * kernel_rows * kernel_cols);
     cudaMemcpy(inbound_kernels, kernels, sizeof(cufftReal) * num_kernels * kernel_rows * kernel_cols, cudaMemcpyHostToDevice);
+
+
+    // assume we can pay the planning price just once and amortize it away, so do the planning up front
+    int32 padded_dimensions[2] = {padded_rows, padded_cols};
+
+    cufftHandle fwd_plan;
+    cufftResult plan_result = cufftPlanMany(&fwd_plan, // plan
+                                            2, // rank
+                                            padded_dimensions, // dimensions
+                                            NULL, 1, 0, NULL, 1, 0, // boilerplate for contiguous access (non-contiguous access not supported now)
+                                            CUFFT_R2C, // type
+                                            num_padded // batch size
+                                           );
+    
+    cufftHandle inv_plan;
+    cufftPlanMany(&inv_plan, // plan
+                  2, // rank
+                  padded_dimensions, // dimensions
+                  NULL, 1, 0, NULL, 1, 0, // boilerplate for contiguous access (non-contiguous access not supported now)
+                  CUFFT_C2R, // type
+                  num_images * num_kernels // batch size
+                 );
+
+
+    // ok, the data is on the gpu; the plans are made; start the timer
+    uint32 num_iterations = 10000;
+    time_t start, end;
+
+    start = time(NULL);
+
+    
+    for(uint32 iteration = 0; iteration < num_iterations; iteration++) {
+    ////////////////////////////////////////////////////////////////////
 
     // rearrange images and kernels to their new padded size, all contiguous
     // to each other, since that is what the batched fft requires right now
@@ -152,21 +201,6 @@ int main(int argc, char *argv[])
 */
     
     // perform forward fft
-    int32 padded_dimensions[2] = {padded_rows, padded_cols};
-
-    cufftHandle fwd_plan;
-    cufftResult plan_result = cufftPlanMany(&fwd_plan, // plan
-                                            2, // rank
-                                            padded_dimensions, // dimensions
-                                            NULL, 1, 0, NULL, 1, 0, // boilerplate for contiguous access (non-contiguous access not supported now)
-                                            CUFFT_R2C, // type
-                                            num_padded // batch size
-                                           );
-    
-    if(plan_result != CUFFT_SUCCESS) {
-        fprintf(stderr, "fwd plan failed: %i", plan_result);
-    }
-
     uint32 transformed_cols = padded_cols / 2 + 1; // only non-redundant complex coefficients are calculated
 
     cufftComplex *transformed;
@@ -192,8 +226,6 @@ int main(int argc, char *argv[])
     }
 */
 
-    cufftDestroy(fwd_plan); // TODO: reuse fft plans
-
     // TODO: memory cleanup as appropriate
     // cudaFree(images);
 
@@ -202,7 +234,6 @@ int main(int argc, char *argv[])
     cudaMalloc((void**)&multiplied, sizeof(cufftComplex) * num_images * num_kernels * padded_rows * transformed_cols);
     cudaMemset(multiplied, 0xFF, sizeof(cufftComplex) * num_images * num_kernels * padded_rows * transformed_cols); // TODO: DEBUGGING ONLY!
 
-    int threads_per_block = 256;
     int blocks_per_grid = (padded_rows * transformed_cols + threads_per_block - 1) / threads_per_block;
     for(uint32 image_index = 0; image_index < num_images; image_index++) {
         for(uint32 kernel_index = 0; kernel_index < num_kernels; kernel_index++) {
@@ -235,39 +266,47 @@ int main(int argc, char *argv[])
 */
 
     // do inverse fft
-    cufftHandle inv_plan;
-    cufftPlanMany(&inv_plan, // plan
-                  2, // rank
-                  padded_dimensions, // dimensions
-                  NULL, 1, 0, NULL, 1, 0, // boilerplate for contiguous access (non-contiguous access not supported now)
-                  CUFFT_C2R, // type
-                  num_images * num_kernels // batch size
-                  );
-
     cufftReal *inverse_transformed;
     cudaMalloc((void**)&inverse_transformed, sizeof(cufftReal) * num_images * num_kernels * padded_rows * padded_cols);
 
     cufftExecC2R(inv_plan, multiplied, inverse_transformed);
 
-    cufftDestroy(inv_plan);
-
-
     // scale the results appropriately (cufft does non-normalized transforms)
-    threads_per_block = 256;
     blocks_per_grid = (num_images * num_kernels * padded_rows * padded_cols + threads_per_block - 1) / threads_per_block;
     elementwise_vector_scalar_multiply_inplace<<<blocks_per_grid, threads_per_block>>>(inverse_transformed,
                                                                                        1.0f / (padded_rows * padded_cols),
                                                                                        num_images * num_kernels * padded_rows * padded_cols);
 
-    //cudaFree(transformed_data); // TODO: free all relevant memory
 
+    cudaFree(fft_input);
+    cudaFree(transformed);
+    cudaFree(multiplied);
+    cudaFree(inverse_transformed);
+    ////////////////////////////////////////////////////////////////////
+    } // end timing-iteration for loop
+
+    // all the calculations are (basically) done, at least for full mode
+    // stop the timer
+    end = time(NULL);
+
+    fprintf(stderr, 
+            "%i %ix%i images, %i %ix%i kernels: %.0fsec elapsed for %i iterations (%f sec/iter)\n",
+            num_images,
+            image_rows,
+            image_cols,
+            num_kernels,
+            kernel_rows,
+            kernel_cols,
+            difftime(end, start),
+            num_iterations,
+            difftime(end, start) / (float)num_iterations);
+
+
+/*
     // TODO: Set strides appropriately or do memcpys to get rid of unneeded padding
     float results[num_images][num_kernels][padded_rows][padded_cols];
     // copy results back to host
     cudaMemcpy(results, inverse_transformed, sizeof(cufftReal) * num_images * num_kernels * padded_rows * padded_cols, cudaMemcpyDeviceToHost);
-
-    //cudaFree(out_data); // todo
-
 
     fprintf(stderr, "OUTBOUND\n");
     for(uint32 image_index = 0; image_index < num_images; image_index++) {
@@ -281,5 +320,9 @@ int main(int argc, char *argv[])
             fprintf(stderr, "\n");
         }
     }
+*/
 
+    // TODO: Other cleanup -- memory freeing, etc.
+    cufftDestroy(fwd_plan); // TODO: reuse fft plans
+    cufftDestroy(inv_plan);
 }
