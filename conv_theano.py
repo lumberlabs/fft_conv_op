c7d6603bfc3ff57cd55ca1b61dcd7206956106d3
Joshua Bleecher Snyder <josh@treelinelabs.com>
2010-12-08 10:34:26 -0500

    spit out output to figure out how the gosh darn thing works

--- conv_theano.py
+++ conv_theano.py
@@ -7,17 +7,27 @@ from theano.tensor.nnet import conv
 
 if __name__ == "__main__":
     batch_size = 1
-    num_images = 50
-    image_dim = 8
-    num_kernels = 20
-    kernel_dim = 5
+    num_images = 4
+    image_dim = 4
+    num_kernels = 2
+    kernel_dim = 2
 
     filter_shape = (num_kernels, num_images, kernel_dim, kernel_dim)
     image_shape = (batch_size, num_images, image_dim, image_dim)
 
     images = T.matrix(dtype=theano.config.floatX)
-    kernels = numpy.ones(filter_shape, dtype=theano.config.floatX)
-    shared_kernels = theano.shared(value=kernels)
+    one_kernel = numpy.ones((kernel_dim, kernel_dim), dtype=theano.config.floatX)
+    kernels = []
+    for x in xrange(num_kernels):
+        sub_kernels = []
+        for y in xrange(num_images):
+            sub_kernels.append(one_kernel * (x + 1) * (y + 1))
+        kernels.append(sub_kernels)
+    test_kernels = numpy.asarray(kernels)
+    print "KERNELS:"
+    print test_kernels
+    print "-------"
+    shared_kernels = theano.shared(value=test_kernels)
 
     reshaped_images = images.reshape((batch_size, num_images, image_dim, image_dim))
     conv_out = conv.conv2d(input=reshaped_images,
@@ -25,10 +35,27 @@ if __name__ == "__main__":
                            filter_shape=filter_shape,
                            image_shape=image_shape,
                            border_mode="full")
-    f = theano.function(inputs=[images], outputs=[conv_out])
+    f = theano.function(inputs=[images], outputs=conv_out)
 
-    test_images = numpy.ones((batch_size, num_images, image_dim, image_dim), dtype=theano.config.floatX).reshape(batch_size * num_images, image_dim * image_dim)
+    one_image = numpy.ones((image_dim, image_dim), dtype=theano.config.floatX)
+    images = []
+    for x in xrange(batch_size):
+        sub_images = []
+        for y in xrange(num_images):
+            sub_images.append(one_image * y)
+        images.append(sub_images)
+    test_images = numpy.asarray(images)
+    # test_images = numpy.ones((batch_size, num_images, image_dim, image_dim), dtype=theano.config.floatX)
+    print "TEST IMAGES"
+    print test_images
+    print "----------"
 
-    for iteration in xrange(10000):
-        f(test_images)
+    test_images = test_images.reshape(batch_size * num_images, image_dim * image_dim)
+
+    print "RESULT (batch_size x num_kernels x convolved_rows x convolved_cols)"
+    print f(test_images)
+    # print f(test_images).shape
+    # output shape is (batch_size, num_kernels, convolved_rows, convolved_cols)
+    # for iteration in xrange(1000):
+    #     f(test_images)
 
